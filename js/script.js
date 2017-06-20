9ead0b2c6c472afc0db9389a2790c3bb910faee4
Denny Azevedo <denny_af@yahoo.com.br>
2017-06-20 15:01:05 -0300

    Subida inicial do site

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).scroll(function(){
+        var topWindow = $ (window).scrollTop();
+        if(topwindow width <780p)
+            {
+                $("#divbusca").addClass('not-show');
+            }
+        else
+        {
+            $("#divbusca").removeClass('not-show');
+        };
+    });
